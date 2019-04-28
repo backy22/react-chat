a6daf0fc565d2f06dd021936f2b5764d2d56cf9f
椿野彩 <tsubakino@backys-MacBook-Pro.local>
2019-04-28 00:33:33 -0400

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBobCkTo65Pv6joqvRTIm5GlS1qSD3_OAA",
+  authDomain: "react-chat-a0dab.firebaseapp.com",
+  databaseURL: "https://react-chat-a0dab.firebaseio.com",
+  projectId: "react-chat-a0dab",
+  storageBucket: "react-chat-a0dab.appspot.com",
+  messagingSenderId: "4662838236"
+};
